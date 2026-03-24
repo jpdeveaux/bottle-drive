bfad545fba012abdde62f6f2b192bd0784aa9f48
Jean-Paul Deveaux <jpdeveaux@gmail.com>
2026-03-24 10:16:08 -0300

    initial revision

---
+++ frontend/src/components/AddressStateSelect.tsx
@@ -0,0 +1,24 @@
+import { Address, AddressState } from '@types';
+
+interface AddressStateProps {
+  addr: Address;
+  handler: (id: string, {state}) => void;
+};
+
+export const AddressStateSelect = ({ addr, handler  }: AddressStateProps) => {
+  return (
+    <select 
+      value={addr.state} 
+      onChange={(e) => {
+        const newState = AddressState[e.target.value];  
+        return handler(addr.id, { state: newState });
+      }}
+      className="cap w-full bg-gray-50 border border-gray-200 rounded-md p-2 text-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+    >
+      {Object.values(AddressState).map((state) => (
+         <option key={state} value={state}>{state}</option>
+        ))
+      }
+    </select>
+  )
+};
