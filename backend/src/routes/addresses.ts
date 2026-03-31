88e618d9e7d5186d3ec31a2951c4b13d5bbf1a87
Jean-Paul Deveaux <jpdeveaux@gmail.com>
2026-03-31 13:10:02 -0300

    added post / route before authenticated routes to accept new addresses from either admin or public

--- backend/src/routes/addresses.ts
+++ backend/src/routes/addresses.ts
@@ -9,7 +9,39 @@ import { Server } from 'socket.io';
 export default (io: Server) => {
   const router = Router();
 
- // all calls here use authenticateJWT and authApproved.
+  router.post('/', async (req, res) => {
+    const { street, notes } = req.body;
+    let geo = await geocodeAddress(street);
+
+    if (!geo || !geo.lat || !geo.lng) {
+      console.log('Geocoding failed for address: '+street);
+      geo = null;
+    }
+
+    try {
+      const newAddress = await prisma.address.create({
+        data: {
+          street: street,
+          lat: geo?.lat || 0,
+          lng: geo?.lng || 0,
+          notes: notes || "",
+          state: "unvisited",
+          zoneId: geo && await checkForZone(geo)
+        },
+        include: ZONE_NAME_AND_USERS
+      });
+
+      io.to('verified').emit('addressUpdated', newAddress);
+      console.log('Address added: ', newAddress);
+
+      res.json({ success: true });
+    } catch (err) {
+      console.log('Error: %s', err);
+      res.status(500).json({ error: "Database error" });
+    }
+  });
+
+ // all calls from here on use authenticateJWT and authApproved.
   router.use(authenticateJWT, authApproved);
 
   // return all addresses
